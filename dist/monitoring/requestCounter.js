ca4929633074de65a5350e62a80d6f3b00489b87
Ishan <ishan03sharma25@gmail.com>
2025-12-01 19:04:57 +0530

    Implemented Basic Histogram using Prometheus

--- dist/monitoring/requestCounter.js
+++ dist/monitoring/requestCounter.js
@@ -14,11 +14,18 @@ const activeRequestsGauge = new prom_client_1.default.Gauge({
     name: 'active_requests',
     help: 'Number of active requests'
 });
+const httpRequestDurationMicroSecondsHistogram = new prom_client_1.default.Histogram({
+    name: 'http_request_duration_ms',
+    help: 'Duration of HTTP requests in ms',
+    labelNames: ['method', 'route', 'status_code'],
+    buckets: [0.1, 5, 15, 50, 100, 200, 300, 400, 500, 1000, 2000, 5000, 10000]
+});
 const requestCountMiddleware = (req, res, next) => {
     const startTime = Date.now();
     activeRequestsGauge.inc();
     res.on('finish', () => {
         const endTime = Date.now();
+        const duration = endTime - startTime;
         console.log(`Request processed in: ${endTime - startTime}ms`);
         //Increment the counter
         requestCounter.inc({
@@ -26,6 +33,11 @@ const requestCountMiddleware = (req, res, next) => {
             route: req.route ? req.route.path : req.path, //Routes like /user
             status_code: res.statusCode //status codes like 200, 404
         });
+        httpRequestDurationMicroSecondsHistogram.observe({
+            method: req.method,
+            route: req.route ? req.route.path : req.path,
+            status_code: res.statusCode
+        }, duration);
         activeRequestsGauge.dec();
     });
     next();
--- src/monitoring/requestCounter.ts
+++ src/monitoring/requestCounter.ts
@@ -12,11 +12,19 @@ const activeRequestsGauge = new client.Gauge({
     help: 'Number of active requests'
 });
 
+const httpRequestDurationMicroSecondsHistogram = new client.Histogram({
+    name: 'http_request_duration_ms',
+    help: 'Duration of HTTP requests in ms',
+    labelNames: ['method', 'route', 'status_code'],
+    buckets: [0.1, 5, 15, 50, 100, 200, 300, 400, 500, 1000, 2000, 5000, 10000]
+})
+
 export const requestCountMiddleware = (req: Request, res: Response, next: NextFunction)=>{
     const startTime = Date.now();
     activeRequestsGauge.inc();
     res.on('finish', ()=>{
         const endTime = Date.now();
+        const duration = endTime-startTime;
         console.log(`Request processed in: ${endTime-startTime}ms`);
 
         //Increment the counter
@@ -25,6 +33,11 @@ export const requestCountMiddleware = (req: Request, res: Response, next: NextFu
             route: req.route ? req.route.path : req.path,//Routes like /user
             status_code: res.statusCode//status codes like 200, 404
         });
+        httpRequestDurationMicroSecondsHistogram.observe({
+            method: req.method,
+            route: req.route ? req.route.path : req.path,
+            status_code: res.statusCode
+        }, duration);
         activeRequestsGauge.dec();
     });
     next();
